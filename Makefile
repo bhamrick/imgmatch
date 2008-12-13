dfbc31bf1f5c5df5082a7464b316b0bad08e09e7
bhamrick <bhamrick@localhost.(none)>
2008-12-13 20:58:33 +0000

    Random image printing

--- Makefile
+++ Makefile
@@ -1,12 +1,18 @@
 CXXFLAGS=-I/usr/include/cairo
 LDFLAGS=-L/usr/lib -lcairo
 
-all: test
+all: test match
 
 test: test.cpp
 	g++ $(CXXFLAGS) -c -o test.o test.cpp
 	g++ $(LDFLAGS) -o test test.o
 	rm test.o
 
+match: match.cpp
+	g++ $(CXXFLAGS) -c -o match.o match.cpp
+	g++ $(LDFLAGS) -o match match.o
+	rm match.o
+
 clean:
 	rm test
+	rm match
--- match.cpp
+++ match.cpp
@@ -6,6 +6,7 @@
 #define NVERT 3
 #define NPOLY 20
 #define PMUT 0.03
+#define NPOP 16
 
 using namespace std;
 
@@ -28,19 +29,19 @@ struct polyimg {
 	polygon poly[NPOLY];
 };
 
-unsigned char* img_data(polyimg *p) {
+unsigned char* img_data(polyimg p) {
 	cairo_surface_t *ans;
-	ans = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, p->w, p->h);
+	ans = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, p.w, p.h);
 	cairo_t *cr;
 	cr = cairo_create(ans);
 	cairo_set_source_rgb(cr,0.0,0.0,0.0);
 	cairo_paint(cr);
 	for(int i = 0; i<NPOLY; i++) {
-		cairo_set_source_rgba(cr,p->poly[i].r,p->poly[i].g,p->poly[i].b,p->poly[i].a);
+		cairo_set_source_rgba(cr,p.poly[i].r,p.poly[i].g,p.poly[i].b,p.poly[i].a);
 		cairo_new_path(cr);
-		cairo_move_to(cr,p->poly[i].x[0],p->poly[i].y[0]);
+		cairo_move_to(cr,p.poly[i].x[0],p.poly[i].y[0]);
 		for(int j = 1; j<NVERT; j++) {
-			cairo_line_to(cr,p->poly[i].x[j],p->poly[i].y[j]);
+			cairo_line_to(cr,p.poly[i].x[j],p.poly[i].y[j]);
 		}
 		cairo_close_path(cr);
 		cairo_fill(cr);
@@ -49,6 +50,28 @@ unsigned char* img_data(polyimg *p) {
 	unsigned char* data = cairo_image_surface_get_data(ans);
 	cairo_surface_destroy(ans);
 	return data;
+}
+
+void save_polyimg(polyimg p, char* foutname) {
+	cairo_surface_t *ans;
+	ans = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, p.w, p.h);
+	cairo_t *cr;
+	cr = cairo_create(ans);
+	cairo_set_source_rgb(cr,0.0,0.0,0.0);
+	cairo_paint(cr);
+	for(int i = 0; i<NPOLY; i++) {
+		cairo_set_source_rgba(cr,p.poly[i].r,p.poly[i].g,p.poly[i].b,p.poly[i].a);
+		cairo_new_path(cr);
+		cairo_move_to(cr,p.poly[i].x[0],p.poly[i].y[0]);
+		for(int j = 1; j<NVERT; j++) {
+			cairo_line_to(cr,p.poly[i].x[j],p.poly[i].y[j]);
+		}
+		cairo_close_path(cr);
+		cairo_fill(cr);
+	}
+	cairo_destroy(cr);
+	cairo_surface_write_to_png(ans,foutname);
+	cairo_surface_destroy(ans);
 }
 
 void init_polygon(polygon& p, int w, int h) {
@@ -73,7 +96,7 @@ void init_polyimg(polyimg& p, int w, int h) {
 void mutate(polyimg& p) {
 	for(int i = 0; i<NPOLY; i++) {
 		if((double)rand()/RAND_MAX < PMUT) {
-			init_polygon(p.poly[i],w,h);
+			init_polygon(p.poly[i],p.w,p.h);
 		}
 	}
 }
@@ -81,7 +104,7 @@ void mutate(polyimg& p) {
 void crossover(polyimg& p1, polyimg p2) {
 	int c = (int)(((double)rand()/RAND_MAX)*NPOLY);
 	for(int i = c; i<NPOLY; i++) {
-		p1.poly[i] = p2.poly[i]
+		p1.poly[i] = p2.poly[i];
 	}
 }
 
@@ -93,5 +116,14 @@ int main(int argc, char** argv) {
 	srand(time(NULL));
 	cairo_surface_t *target = cairo_image_surface_create_from_png(argv[1]);
 	unsigned char* tdata = cairo_image_surface_get_data(target);
-	
+	int w = cairo_image_surface_get_width(target), h = cairo_image_surface_get_height(target);
+	polyimg pop[NPOP];
+	for(int i = 0; i<NPOP; i++) init_polyimg(pop[i],w,h);
+	for(int i = 0; i<NPOP; i++) {
+		char foutname[20];
+		sprintf(foutname,"img%d.png",i);
+		save_polyimg(pop[i],foutname);
+		printf("%d: %.3lf\n",i,diff(img_data(pop[i]),tdata,w*h*4));
+	}
+	return 0;
 }
